a9d9eb216f96c492e8bae3605b19d30cdd91ab56
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2026-05-08 10:49:47 +0300

    refactor: correct formatting and update way of building API URL

--- script.js
+++ script.js
@@ -12,24 +12,26 @@ const date = document.getElementById("date");
 const showDate = () => {
     const now = new Date();
     const day = now.getDate();
-    const monthName = now.toLocaleString('en', {month: 'long'});
+    const monthName = now.toLocaleString("en", { month: "long" });
     date.innerHTML = `${day} ${monthName}`;
-}
+};
 
 async function getWeather(city) {
     city = city.trim();
-    if(!city) return;
+    if (!city) return;
 
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`);
+        const url = new URL("https://api.openweathermap.org/data/2.5/weather");
+        url.searchParams.append("q", city);
+        url.searchParams.append("appid", apikey);
+        url.searchParams.append("units", "metric");
 
-        if(!response.ok) throw new Error("City not found");
+        const response = await fetch(url);
+        if (!response.ok) throw new Error("City not found");
 
         const result = await response.json();
 
-        console.log(result);
-
-        card.classList.add('active')
+        card.classList.add("active");
         card.style.height = "530px";
 
         const iconUrl = `https://openweathermap.org/img/wn/${result.weather[0].icon}@4x.png`;
@@ -39,28 +41,27 @@ async function getWeather(city) {
         humidity.innerHTML = `${result.main.humidity} %`;
         wind.innerHTML = `${result.wind.speed} m/s`;
         temp.innerHTML = `${Math.round(result.main.temp)} °C`;
-
-    } catch (err){
+    } catch (err) {
         alert(err);
-}
-}
-
-searchBtn.addEventListener('click',() => {
-    getWeather(searchBox.value);
-});
-
-searchBox.addEventListener('keypress',(e) => {
-    if(e.key === "Enter") {
-    getWeather(searchBox.value);
-}
-});
-
-document.addEventListener('click', (e) => {
-    if(card.classList.contains(`active`) && !card.contains(e.target)){
-        card.classList.remove('active');
-        card.style.height = "230px"
-        searchBox.value = ""
     }
-})
+}
+
+searchBtn.addEventListener("click", () => {
+    getWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        getWeather(searchBox.value);
+    }
+});
+
+document.addEventListener("click", (e) => {
+    if (card.classList.contains("active") && !card.contains(e.target)) {
+        card.classList.remove("active");
+        card.style.height = "230px";
+        searchBox.value = "";
+    }
+});
 
 showDate();
